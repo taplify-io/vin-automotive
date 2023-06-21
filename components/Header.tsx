4d622d5e9ed8bc1c341ee096b1f0513153c375e8
yapyeeqiang <yapyeeqiang@gmail.com>
2023-06-21 16:13:21 +0800

    fix: cta

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,12 +8,12 @@ function Header() {
             </div>
 
             <div className="ml-7 flex items-center gap-2">
-                <a href={generateWhatsAppLink('60194513111', "I'm interested to know the available car plates.")} target="_blank" className="flex items-center gap-3 bg-gradient-to-r to-[#FFD700] via-[#FFC200] from-[#FFA500] rounded-lg py-2.5 px-4">
+                <a href={generateWhatsAppLink('60194513111', "I'm interested to know the available car plates.")} target="_blank" className="flex items-center hover:scale-105 transition-all gap-3 bg-gradient-to-r to-[#FFD700] via-[#FFC200] from-[#FFA500] rounded-lg py-2.5 px-4">
                     <img className="h-4 w-4" src="/assets/black-whatsapp.png" alt="" />
                     <span className="text-sm font-medium">Contact Alan</span>
                 </a>
 
-                <a href={generateWhatsAppLink('60122697878', "I'm interested to know the available car plates.")} target="_blank" className="flex items-center gap-3 bg-gradient-to-r to-[#FFD700] via-[#FFC200] from-[#FFA500] rounded-lg py-2.5 px-4">
+                <a href={generateWhatsAppLink('60122697878', "I'm interested to know the available car plates.")} target="_blank" className="flex items-center hover:scale-105 transition-all gap-3 bg-gradient-to-r to-[#FFD700] via-[#FFC200] from-[#FFA500] rounded-lg py-2.5 px-4">
                     <img className="h-4 w-4" src="/assets/black-whatsapp.png" alt="" />
                     <span className="text-sm font-medium">Contact Alvin</span>
                 </a>
